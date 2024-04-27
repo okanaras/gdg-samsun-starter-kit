292094519e22e41b18d41166ab2c2e55e3c98e13
Okan Aras <oknaras@icloud.com>
2024-04-27 16:11:53 +0300

    Bolum 20: Laravel ve Eticaret Projesi Gelistirme Adimlari - 129. Observe ile Güncellenen Modelde İşlem Yapma - Model Query İyileştirmesi

--- projects/eticaret/app/Observers/UserObserver.php
+++ projects/eticaret/app/Observers/UserObserver.php
@@ -26,7 +26,10 @@ class UserObserver
      */
     public function updated(User $user): void
     {
-        //
+        // isDirty() bir alan değiştiğinde kontrol eder.
+        if ($user->isDirty('email_verified_at')) {
+            Cache::forget('verify_token_' . request()->token);
+        }
     }
 
     /**
